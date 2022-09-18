e6d8abc3a2a2caf6f041f930a7b03d2dc5efde05
J1min <jmhong101506@gmail.com>
2022-09-18 18:11:33 +0900

    ADD :: 각종 주석 추가

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -7,36 +7,29 @@ let cachedClient: MongoClient;
 let cachedDb: Db;
 
 export async function connectToDatabase() {
-  // check the cached.
   if (cachedClient && cachedDb) {
-    // load from cache
     return {
       client: cachedClient,
       db: cachedDb,
     };
   }
 
-  // set the connection options
   const opts = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   };
 
-  // check the MongoDB URI
   if (!MONGODB_URI) {
     throw new Error("Define the MONGODB_URI environmental variable");
   }
-  // check the MongoDB DB
   if (!MONGODB_DB) {
     throw new Error("Define the MONGODB_DB environmental variable");
   }
 
-  // Connect to cluster
   let client = new MongoClient(MONGODB_URI);
   await client.connect();
   let db = client.db(MONGODB_DB);
 
-  // set cache
   cachedClient = client;
   cachedDb = db;
 
