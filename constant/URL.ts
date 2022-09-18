0f17731cfe6e49bd254a28937b5608933344c41c
J1min <jmhong101506@gmail.com>
2022-09-18 18:17:40 +0900

    FIX :: URL 변경

--- constant/URL.ts
+++ constant/URL.ts
@@ -1,6 +1,8 @@
-export const GET_URL = "http://localhost:3000/api/getData";
-export const GET_VOTE_URL = "http://localhost:3000/api/getVoteData";
-export const POST_URL = "http://localhost:3000/api/postData";
-export const DELETE_URL = "http://localhost:3000/api/deleteAllData";
+const URL = "https://chung-ryeol-student-council.vercel.app/";
 
-export const ADMIN_PASSWORD = "minseo";
+export const GET_URL = `${URL}/getData`;
+export const GET_VOTE_URL = `${URL}/getVoteData`;
+export const POST_URL = `${URL}/postData`;
+export const DELETE_URL = `${URL}/deleteAllData`;
+
+export const ADMIN_PASSWORD = `minseo`;
