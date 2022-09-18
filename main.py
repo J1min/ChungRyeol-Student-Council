e511ab384ae3b8f988ac046fa719d08e16fa14e2
J1min <jmhong101506@gmail.com>
2022-09-18 14:45:26 +0900

    Change :: Server -> Next.js api

--- main.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
