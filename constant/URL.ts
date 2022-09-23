36531650a6f3126c0c71cf4676ee2843782f9de8
J1min <jmhong101506@gmail.com>
2022-09-23 10:01:17 +0900

    DELETE :: constant

--- constant/URL.ts
+++
@@ -1,9 +0,0 @@
-const URL = "https://crhs-council.vercel.app/api";
-
-export const GET_URL = `${URL}/getData`;
-export const GET_VOTE_URL = `${URL}/getVoteData`;
-export const POST_URL = `${URL}/postData`;
-export const DELETE_URL = `${URL}/deleteAllData`;
-
-export const ADMIN_PASSWORD = `minseo`;
-export const ADMIN_VOTE_PASSWORD = `msms`;
