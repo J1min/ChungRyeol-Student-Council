f63bb9d30848bf2515a19e1a942204f75e295896
J1min <jmhong101506@gmail.com>
2022-09-24 13:51:34 +0900

    FIX :: font-size 수정

--- styles/style.ts
+++ styles/style.ts
@@ -14,7 +14,7 @@ export const Container = styled.div`
 export const StyledLi = styled.p`
   list-style: none;
   text-align: center;
-  font-size: 5rem;
+  font-size: 4rem;
 `;
 
 export const StyledUl = styled.ul`
